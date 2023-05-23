6f5c51193fe2f368cafa23474b214c86373acf50
minhtran <dannitran99@gmail.com>
2023-05-23 17:59:16 +0700

    first route

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"propertiesGo/pkg/handler"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var collection *mongo.Collection
 
 func homeLink(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, "Welcome home!")
 }
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ := mongo.Connect(ctx, clientOptions)
+	collection = client.Database("example").Collection("people")
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/api/news", handler.GetAllTodo).Methods(http.MethodGet)
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
