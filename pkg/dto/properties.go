9c618822b5bf6cdd03de329f72b37ebce400f253
minh.tran <dannitran99@gmail.com>
2023-06-06 15:49:30 +0700

    api change

--- pkg/dto/properties.go
+++ pkg/dto/properties.go
@@ -4,6 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Properties struct {
 	ID        primitive.ObjectID `bson:"_id"`
+	Picture   []string 			 `json:"picture"`
 	Name      string             `json:"name"`
 	Price     int64              `json:"price"`
 	Area      int32              `json:"area"`
